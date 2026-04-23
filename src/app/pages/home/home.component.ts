00d6fcb5243f4a4052e243c8ae2c008f230f10f1
selestino <selestinoolivier@gmail.com>
2026-04-23 23:36:44 +0300

    @selestinohajaniaina fix asyncing to get data

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,6 +3,7 @@ import { ArticleCardComponent } from '../../components/article-card/article-card
 import { Article } from '../../article';
 import { LitleArticleCardComponent } from '../../components/litle-article-card/litle-article-card.component';
 import { SessionService } from '../../service/session.service';
+import { ArticleService } from '../../service/article.service';
 
 @Component({
   selector: 'app-home',
@@ -18,10 +19,22 @@ export class HomeComponent {
   public articles!: Article[];
 
 
-  constructor(private sessionService: SessionService) {
+  constructor(private articleService: ArticleService, private sessionService: SessionService) {
     this.articles = this.sessionService.getArticles();
     this.topArticle = this.sessionService.getArticleMostViewed()!;
   }
 
+  ngOnInit(): void {
+    if(!this.articles || this.articles.length === 0) {
+      this.articleService.getAllArticles()
+      .subscribe((response: any) => {
+        if (response && response.success) {
+          this.articles = response.data;
+          this.sessionService.storeArticles(this.articles);
+          this.topArticle = this.sessionService.getArticleMostViewed()!;
+        }
+      });
+    }
+  }
   
 }
